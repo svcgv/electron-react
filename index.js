da534a718abc5214932a4f2304757861f7c28460
heben <hb1230123@hotmail.com>
2020-06-29 17:57:26 +0800

    feat: 修复打包和启动冲突问题

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-const { app, BrowserWindow } = require('electron')
-
-function createWindow () {   
-  // 创建浏览器窗口
-  const win = new BrowserWindow({
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const env = process.env.NODE_ENV
+console.log('process.env===',env)
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // 并且为你的应用加载index.html
-  win.loadFile('./bundle/index.html')
-// win.loadURL('http://localhost:3000')
-
-  // 打开开发者工具
-  win.webContents.openDevTools()
+  // and load the index.html of the app.
+  if(env === 'dev'){
+    mainWindow.loadURL('http://localhost:3000')
+  }else{
+    mainWindow.loadFile('./bundle/index.html')
+  }
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
-// Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
-// 部分 API 在 ready 事件触发后才能使用。
-app.whenReady().then(createWindow)
-
-//当所有窗口都被关闭后退出
-app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
-app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
