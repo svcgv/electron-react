cb747d174607e266a470f33ba2c9f517c24065eb
heben <hb1230123@hotmail.com>
2020-07-10 18:08:15 +0800

    feat:密钥合并优化

--- content/src/utils/password.js
+++ content/src/utils/password.js
@@ -2,10 +2,34 @@ import CryptoJS from 'crypto-js'
 
 var key = 'aPasswordfa'
 var iv = '12345678abcdefgh'
-export function getPassword (commonwords, attribute) {
-  return encrypt(attribute + commonwords)
+export function getPassword (commonwords = '', attribute = '', maxSize = 12) {
+  const commonArr = commonwords.split('')
+  const attributeArr = attribute.split('')
+  let arr = []
+  if (commonArr.length > attributeArr.length) {
+    arr = mergeTwoArr(attributeArr, commonArr)
+  } else {
+    arr = mergeTwoArr(commonArr, attributeArr)
+  }
+  const fullPassword = encrypt(arr.join(''))
+  return `${fullPassword}`.slice(fullPassword.length - maxSize, maxSize)
 }
 
+function mergeTwoArr (smallArray = [], bigArray = []) {
+  const arr = []
+  for (let i = 0; i < bigArray.length; i++) {
+    const a = bigArray.pop()
+    const b = smallArray.pop()
+    if (i % 2 === 1) {
+      a && arr.push(a)
+      b && arr.push(b)
+    } else {
+      a && arr.unshift(a)
+      b && arr.unshift(b)
+    }
+  }
+  return arr
+}
 function encrypt (text) {
   return CryptoJS.AES.encrypt(text, CryptoJS.enc.Utf8.parse(key), {
     iv: CryptoJS.enc.Utf8.parse(iv),
